30379ac0fb17ca562ff6c7ad813618266d8092cb
remind1990 <tigerstylest00@gmail.com>
2023-06-04 17:14:03 -0230

    bug fix with headers and in login

--- public/js/login.js
+++ public/js/login.js
@@ -6,7 +6,7 @@ export const login = async (email, password) => {
   try {
     const res = await axios({
       method: 'POST',
-      url: 'http://localhost:4400/api/v1/users/login',
+      url: '/api/v1/users/login',
       data: {
         email,
         password
