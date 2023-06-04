8ba58b1f8d5f175943017549150543d98d220bbe
remind1990 <tigerstylest00@gmail.com>
2023-06-04 19:27:55 -0230

    fix in bookingController

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -15,7 +15,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
     mode: 'payment',
     success_url: `${req.protocol}://${req.get(
       'host'
-    )}/my-tours/alert=booking`,
+    )}/my-tours?alert=booking`,
     cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
     customer_email: req.user.email,
     client_reference_id: req.params.tourId,
