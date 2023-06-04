0171ebae2f185aedb7742dc2991e2921fc878ad7
remind1990 <tigerstylest00@gmail.com>
2023-06-04 17:56:13 -0230

    bug with wrong secret

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -61,7 +61,7 @@ exports.webhookCheckout = (req, res, next) => {
     event = stripe.webhooks.constructEvent(
       req.body,
       sig,
-      process.env.WEBHOOK_SECRET
+      process.env.STRIPE_WEBHOOK_SECRET
     );
   } catch (err) {
     return res.status(400).send(`Webhook Error ${err.message}`);
