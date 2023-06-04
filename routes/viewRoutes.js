9dedd01ce2dd09a4465e36e5eedf598cafeac568
remind1990 <tigerstylest00@gmail.com>
2023-06-04 19:21:46 -0230

    Implemented stripe messages

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -3,7 +3,7 @@ const viewsController = require('../controllers/viewsController');
 const authController = require('../controllers/authController');
 
 const router = express.Router();
-
+router.use(viewsController.alerts);
 router.get('/', authController.isLoggedIn, viewsController.getOverview);
 router.get('/tour/:slug', authController.isLoggedIn, viewsController.getTour);
 router.get('/login', authController.isLoggedIn, viewsController.getLoginForm);
