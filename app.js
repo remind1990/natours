c55bd758bc5cd9b3bee9986e1aca931adb29285f
remind1990 <tigerstylest00@gmail.com>
2023-06-04 14:35:31 -0230

    added cors

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -25,7 +26,16 @@ app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
 // 1) GLOBAL MIDDLEWARES
+
+// Implement CORD
+app.use(cors());
+// api.natours.com(backk end app)  natours.com(front end app)
+// app.use(cors({
+//   origin: 'https://www.natours.com
+// }))
 // Serving static files
+app.options('*', cors());
+// or specific route only app.options('/api/vi/tours/:id', cors()); 
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Set security HTTP headers
